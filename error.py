a828924ee897cb302eb935e086097d53b4595879
Moots <75674471+Mootss@users.noreply.github.com>
2021-01-17 13:12:46 +0500

    add httperror 500

--- error.py
+++ error.py
@@ -8,3 +8,8 @@ class InvalidArgument(mvWeatherException):
 # when an invalid station name is passed in 
 
     pass
+
+class HTTPError500(mvWeatherException):
+# Error 500, internal server error 
+
+    pass
